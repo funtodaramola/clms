a82c19e2a824da21c526bb29212eb822ff34a4d1
Funto Daramola <fundaramola2013@gmail.com>
2018-06-05 16:59:55 +0100

    added signup link and required styling

--- index.php
+++ index.php
@@ -5,17 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
+    
     <title>Caleb Library Management System</title>
 </head>
 <body>
     <!--Create a modal box for login in future-->
     <!--Login form-->
     <div class="login">
-        <form>
+        <form action="app/index.php" method="post">
             <input id=username type="text" name="username" placeholder="Username" required>
             <input id=password type="password" name="password" placeholder="Password" required>
-            <input type="submit" value="Login">
+            <input type="submit" name="login" value="Login">
         </form>
+        <p class="signup">New user? <a href="#">Sign up</a></p>
     </div>
     
 </body>
