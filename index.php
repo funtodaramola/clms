c0613ca852134ffc6caaf61381ea5366e0f1fa18
Funto Daramola <fundaramola2013@gmail.com>
2018-06-05 15:27:13 +0100

    introduced php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Caleb Library Management System</title>
+</head>
+<body>
+    <!--Create a modal box for login in future-->
+    <!--Login form-->
+    <div class="login">
+        <form>
+            <input id=username type="text" name="username" placeholder="Username" required>
+            <input id=password type="password" name="password" placeholder="Password" required>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+    
+</body>
+</html>
