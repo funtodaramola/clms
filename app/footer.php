3dfc4b47e6162373feec43968784f4bb9ed790ab
Funto Daramola <fundaramola2013@gmail.com>
2018-06-11 14:51:59 +0100

    created head and foot include page

---
+++ app/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <span>&copy; Caleb University || Library</span>
+    <div class="logout">
+        <span>
+            <!-- <?php 
+            $username = $_POST["username"];
+            echo "Logged in as {$username}";
+            ?> -->
+
+            <!-- Need to fix this after connecting to database of registered users -->
+        </span>
+    </div>
+</footer>
---
+++ app/header.php
@@ -0,0 +1,6 @@
+<header></header>
+    <div class="feedback">
+        <span>
+            <!-- feedback based on action performed -->
+        </span>
+    </div>
