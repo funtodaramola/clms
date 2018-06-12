a79de5584acc88be120708867ddb642380001964
Funto Daramola <fundaramola2013@gmail.com>
2018-06-12 10:40:57 +0100

    added html for add new modal in books

--- app/books.php
+++ app/books.php
@@ -15,15 +15,58 @@
 
         <section class="books-section">
 
-            <div class="books-top">
-                <button onclick="document.getElementById('').style.display='block'" class="new-icon"><img src="../img/new.png" alt="new"></button>
-                <div class="search-container">
-                    <form action="#">
-                        <input type="text" placeholder="Search..." name="search">
-                        <button type="submit"><img src="../img/search.png" alt="search"></button>
-                    </form>
-                </div>
+        <div class="books-top">
+            <button onclick="document.getElementById('add-modal').style.display='block'" class="new-icon"><img src="../img/new.png" alt="new"></button>
+            <div class="search-container">
+                <form action="#">
+                    <input type="text" placeholder="Search..." name="search">
+                    <button type="submit"><img src="../img/search.png" alt="search"></button>
+                </form>
             </div>
+        </div>
+
+        <!-- The Modal (contains add new book form) -->
+        <div id="add-modal" class="add-modal">
+            <!-- span to close the modal -->
+            <span onclick="document.getElementById('add-modal').style.display='none'" class="close-modal" title="Close Modal">&times;</span>
+            
+            <!-- try using js to add content of form -->
+            <form class="modal-content" action="#">
+                <div class="form-content">
+                    <h1>Add Book</h1>
+                    <hr>
+                    <label for="title"><b>Title</b></label>
+                    <input type="text" placeholder="Title..." name="title" class="addnew-input" required>
+
+                    <label for="author"><b>Author</b></label>
+                    <input type="text" placeholder="Author's name..." name="author" class="addnew-input" required>
+
+                    <label for="publisher"><b>Publisher</b></label>
+                    <input type="text" placeholder="Publisher..." name="publisher" class="addnew-input" required>
+
+                    <select>
+                        <option value="000">Information/General Studies</option>
+                        <option value="100">Philosophy/Psychology</option>
+                        <option value="200">Religion</option>
+                        <option value="300">Social Science</option>
+                        <option value="400">Language</option>
+                        <option value="500">Science</option>
+                        <option value="600">Technology/Applied Science</option>
+                        <option value="700">Arts/Humanity</option>
+                        <option value="800">Literature</option>
+                        <option value="900">History/Geography</option>
+                    </select>
+                    
+                    <label for="edition"><b>Edition</b></label>
+                    <input type="text" placeholder="Edition..." name="edition" class="addnew-input" required>
+
+                    <div class="btns">
+                        <button type="button" onclick="document.getElementById('add-modal').style.display='none'" class="cancelbtn">Cancel</button>
+                        <button type="submit" class="addbtn">Add</button>
+                    </div>
+                </div>
+            </form>
+        </div>
 
         </section>
 
