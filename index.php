eec56ed70001c64cb0e86096b64f883ce5373103
Funto Daramola <fundaramola2013@gmail.com>
2018-06-13 10:56:08 +0100

    added jquery script link

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
+    <script src="js/jquery-3.3.1.min.js"></script>
     <title>Caleb Library Management System</title>
     <style>
     </style>
