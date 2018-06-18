6059a6ca13656e96391938ce111840f3e54d8e42
Funto Daramola <fundaramola2013@gmail.com>
2018-06-18 10:47:08 +0100

    made page able to add students to db

--- app/students.php
+++ app/students.php
@@ -1,5 +1,25 @@
 <?php require ('db.php')?>
+<?php
+    //check if form is submitted
+    if (isset($_POST['add'])) {
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $dept = $_POST['dept'];
+        $college = $_POST['college'];
+        $level = $_POST['level'];
+        $response = "Successfully added Student";
 
+        // adding students to db
+        $update_query = "INSERT INTO students (";
+        $update_query .= " fname, lname, dept, college, level";
+        $update_query .= ") VALUES (";
+        $update_query .= " '{$fname}', '{$lname}', '{$dept}', '{$college}', {$level}";
+        $update_query .= ")";
+        $updated = mysqli_query($connection, $update_query);
+    } else {
+        $response = null;
+    }
+?>
 <?php
 	//Perform database query
 	$select_query  = "SELECT * FROM students ";
@@ -36,13 +56,13 @@
             </div>
         </div>
 
-        <!-- The Modal (contains add new book form) -->
+        <!-- The Modal (contains add new student form) -->
         <div id="modal-form" class="modal-form">
             <!-- span to close the modal -->
             <span onclick="document.getElementById('modal-form').style.display='none'" class="close-modal" title="Close Modal">&times;</span>
             
             <!-- try using js to add content of form -->
-            <form class="modal-content" action="#">
+            <form class="modal-content" action="students.php" method="post">
                 <div class="form-content">
                     <h1>Add Students</h1>
                     <hr>
@@ -55,6 +75,7 @@
                     <label for="dept"><b>Department</b></label>
                     <input type="text" placeholder="Department..." name="dept" class="input-field" required>
 
+                    <!-- link department to college later -->
                     <label for="college"><b>College</b></label>
                     <select name="college">
                         <option value="copas">College of Pure and Applied Science</option>
@@ -68,13 +89,24 @@
 
                     <div class="btns">
                         <button type="button" onclick="document.getElementById('modal-form').style.display='none'" class="cancelbtn">Cancel</button>
-                        <button type="submit" class="submit">Add</button>
+                        <button type="add" class="submit" name="add" value="add">Add</button>
                     </div>
                 </div>
             </form>
         </div>
         <div class="main">
-
+            <?php 
+            // update students table
+                if (isset($_POST["add"])) {
+                    // test for error
+                if ($updated) {
+                    // success
+                    echo "{$response}";
+                } else {
+                    die("Students update failed. " . mysqli_error($connection));
+                }
+                }
+            ?>
             <?php
                 //Release returned data
                 mysqli_free_result($selected);
