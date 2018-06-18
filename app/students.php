33f4d4ea5ca77542bb7067c02d7f148e893a3394
Funto Daramola <fundaramola2013@gmail.com>
2018-06-18 11:36:50 +0100

    relocated college input coe

--- app/students.php
+++ app/students.php
@@ -71,10 +71,19 @@
 
                     <label for="lname"><b>Last Name</b></label>
                     <input type="text" placeholder="Last name..." name="lname" class="input-field" required>
-
+                    
+                    <label for="college"><b>College</b></label>
+                    <select name="college">
+                        <option value="copas">College of Pure and Applied Science</option>
+                        <option value="cosomas">College of Social and Management Science</option>
+                        <option value="colensma">College of Environment Science and Management</option>
+                    </select>
+                    
+                    <!-- link department to college later -->
                     <label for="dept"><b>Department</b></label>
                     <select name="dept">
                         <!-- cosomas -->
+                        <span id="cosomas">
                         <option value="ACC">Accounting</option>
                         <option value="BUS">Business Administration</option>
                         <option value="ECO">Economics</option>
@@ -82,7 +91,9 @@
                         <option value="POS">Political Science</option>
                         <option value="IRS">International Relations</option>
                         <option value="CRM">Criminology, Peace and Conflict Resolution</option>
+                        </span>
                         <!-- copas -->
+                        <span id="copas">
                         <option value="MCB">MicroBiology</option>
                         <option value="BCH">BioChemistry</option>
                         <option value="ICH">Industrial Chemistry</option>
@@ -90,22 +101,15 @@
                         <option value="PHY">Physics</option>
                         <option value="MAS">Mass Communication</option>
                         <option value="PHY">Physics</option>
+                        </span>
                         <!-- colensma -->
+                        <span id="copas">
                         <option value="ARC">Architecture</option>
                         <option value="BUD">Building</option>
                         <option value="SUY">Quantity Surveying</option>
+                        </span>
                     </select>
                     <!-- <input type="text" placeholder="Department..." name="dept" class="input-field" required> -->
-
-                    <!-- link department to college later -->
-                    <label for="college"><b>College</b></label>
-                    <select name="college">
-                        <option value="copas">College of Pure and Applied Science</option>
-                        <option value="cosomas">College of Social and Management Science</option>
-                        <option value="colensma">College of Environment Science and Management</option>
-                        <option value="copos">College of Postgraduate Studies</option>
-                    </select>
-                    
                     <label for="level"><b>Level</b></label>
                     <input type="number" name="level" class="input-field" required>
 
