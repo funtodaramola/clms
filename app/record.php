c26cffc405638fa4e4531f4a2a6e1d27f7a457be
Funto Daramola <fundaramola2013@gmail.com>
2018-06-20 15:39:20 +0100

    created initial query to and stored result in an array

--- app/record.php
+++ app/record.php
@@ -1,4 +1,14 @@
 <?php require ('db.php')?>
+<?php
+	//Perform database query
+    $select_query  = "SELECT * FROM issued ";
+    $select_query  .= "ORDER BY book_id DESC";
+	$selected = mysqli_query($connection, $select_query);
+	// Test if there was a query error
+	if (!$selected) {
+		die("Database query failed.");
+	}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,7 +24,18 @@
     <div class="container">
         <?php include ('header.php')?>
         <section class="record-section">
-
+            <?php
+            $record = array();
+            while($issued_books = mysqli_fetch_assoc($selected)){
+                $record[] = $issued_books;
+            }
+            ?>
+            <pre><?php  print_r($record);?></pre>
+            
+            <?php
+                //Release returned data
+                mysqli_free_result($selected);
+            ?>
         </section>
         <?php include ('footer.php'); ?>
     </div>
@@ -24,8 +45,3 @@
   //Close database connection
   mysqli_close($connection);
 ?>
-<!-- <?php 
-$book_info[] = $book_data;
-                print_r($book_info);
-                
-?> -->
