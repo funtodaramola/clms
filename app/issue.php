8141a6a7fbd404fadaebe249866147c08935020a
Funto Daramola <fundaramola2013@gmail.com>
2018-06-20 12:12:51 +0100

    added date input field

--- app/issue.php
+++ app/issue.php
@@ -51,6 +51,9 @@
                     <label for="libraryNo"><b>Library Number</b></label>
                     <input type="text" placeholder="Library Number..." name="libraryNo" class="input-field" required>
 
+                    <label for="datedue"><b>Date Due</b></label>
+                    <input type="date" name="datedue" min="2018-01-01">
+
                     <button type="submit" class="submit" name= "issue" value="issue"><span id="issueBtn">Issue</span></button>
                 </div>
             </form>
