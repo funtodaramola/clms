a175939bb62026e4a082485aa0c16e17b23a51e7
Funto Daramola <fundaramola2013@gmail.com>
2018-06-30 11:36:33 +0100

    removed signup modal from login page

--- public/index.php
+++ public/index.php
@@ -20,37 +20,7 @@
             <input id=username type="text" name="username" placeholder="Username" required>
             <input id=password type="password" name="password" placeholder="Password" required>
             <input type="submit" name="login" value="Login">
-        </form>
-        <!-- Button to open the modal -->
-        <p class="signup">New user? <button onclick="document.getElementById('id01').style.display='block'" class="btn">Sign up</button></p>
-    </div>
-
-    <!-- The Modal (contains the Sign Up form) -->
-    <div id="id01" class="modal">
-        <!-- span to close the modal -->
-        <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-        <form class="modal-content" action="index.php">
-            <div class="container">
-                <h1>Sign Up</h1>
-                <hr>
-                <label for="fname"><b>Full name</b></label>
-                <input type="text" placeholder="Enter Full name" name="fname" class="signup-input" required>
-
-                <label for="username"><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="username" class="signup-input" required>
-
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="password" class="signup-input" required>
-                <!-- add password repeat later -->
-                <!-- <label for="psw-repeat"><b>Repeat Password</b></label>
-                <input type="password" placeholder="Repeat Password" name="psw-repeat" required> -->
-
-                <div class="btns">
-                    <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-                    <button type="submit" class="signupbtn">Sign Up</button>
-                </div>
-            </div>
-        </form>
+        </form>  
     </div>
     
 </body>
