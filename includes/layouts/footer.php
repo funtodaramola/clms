68b13544d3ae9a7c28182ea84036aebce8ba05c3
nathan <oluwaseyieniayomi@gmail.com>
2018-07-02 16:40:25 +0100

    changes

--- includes/layouts/footer.php
+++ includes/layouts/footer.php
@@ -2,7 +2,57 @@
     <span>&copy; Caleb University || Library</span>
     <div class="logout">
         <span>
-            <button class="add-admin">Account Settings</button>
+            <button id="myBtn" class="add-admin">Account Settings</button>
+			<div id="myModal" class="modal">
+			<div class="modal-content">
+    <span class="close">&times;</span>
+	<form class="modal-content" action="#" method="post">
+            <div class="form-content">
+                <h1>Login</h1>   
+                <label for="username"><b>Username</b></label>
+                <input type="text" placeholder="Username" name="username" class="input-field" required>
+
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Password" name="password" class="input-field" required>
+
+                <div class="btns">
+                    <button type="button" onclick="document.getElementById('add-admin').style.display='none'" class="cancelbtn">Cancel</button>
+                    <button type="submit" class="submit">Add</button>
+                </div>
+            </div>
+        </form>
+  </div>
+
+</div>
+<script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+} 
+
+</script>
+
             <!-- Button to open the modal -->
             <button onclick="document.getElementById('add-admin').style.display='block'" class="add-admin">Add Admin</button>
             <!-- TODO add manage admins page-->
