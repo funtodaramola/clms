46bfa6b6a834ae67fa044eba360642c3f34de11e
Funto Daramola <fundaramola2013@gmail.com>
2018-07-05 21:50:30 +0100

    added link to manage.php to footer

--- includes/layouts/footer.php
+++ includes/layouts/footer.php
@@ -4,35 +4,12 @@
         <span>
             <!-- Buttons to open the modal -->
             <button id="myBtn" onclick="document.getElementById('account').style.display='block'" class="foot_btn">Account Settings</button>
-            <button onclick="document.getElementById('add-admin').style.display='block'" class="foot_btn">Add Admin</button>
+            <button class="foot_btn"><a href="../../public/app/manage.php">Manage Admin</a></button>
             <button class="foot_btn">Logout</button>
         </span>
     </div>
 
-    <!-- The Modal (contains the Add admin form) -->
-    <div id="add-admin" class="modal-form">
-        <!-- span to close the modal -->
-        <span onclick="document.getElementById('add-admin').style.display='none'" class="close-modal" title="Close Modal">&times;</span>
-        <form class="modal-content" action="#" method="post">
-            <div class="form-content">
-                <h1>New Admin</h1>
-                <hr>
-                <label for="fullname"><b>Full name</b></label>
-                <input type="text" placeholder="Enter Full name..." name="fullname" class="input-field" required>
-
-                <label for="username"><b>Username</b></label>
-                <input type="text" placeholder="Username will be generated..." name="username" class="input-field" required>
-
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Password will be generated..." name="password" class="input-field" required>
-
-                <div class="btns">
-                    <button type="button" onclick="document.getElementById('add-admin').style.display='none'" class="cancelbtn">Cancel</button>
-                    <button type="submit" class="submit">Add</button>
-                </div>
-            </div>
-        </form>
-    </div>
+    
 
 <!-- The Modal (contains the Account settings form) -->
     <div id="account" class="modal-form">
--- public/app/manage.php
+++ public/app/manage.php
@@ -1,7 +1,32 @@
 <?php include ('../../includes/layouts/header.php')?>
 
-<section class="">
+<section class="manage-section">
+    <button onclick="document.getElementById('add-admin').style.display='block'" class="foot_btn">Add Admin</button>
 
+    <!-- The Modal (contains the Add admin form) -->
+    <div id="add-admin" class="modal-form">
+        <!-- span to close the modal -->
+        <span onclick="document.getElementById('add-admin').style.display='none'" class="close-modal" title="Close Modal">&times;</span>
+        <form class="modal-content" action="#" method="post">
+            <div class="form-content">
+                <h1>New Admin</h1>
+                <hr>
+                <label for="fullname"><b>Full name</b></label>
+                <input type="text" placeholder="Enter Full name..." name="fullname" class="input-field" required>
+
+                <label for="username"><b>Username</b></label>
+                <input type="text" placeholder="Username will be generated..." name="username" class="input-field" required>
+
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Password will be generated..." name="password" class="input-field" required>
+
+                <div class="btns">
+                    <button type="button" onclick="document.getElementById('add-admin').style.display='none'" class="cancelbtn">Cancel</button>
+                    <button type="submit" class="submit">Add</button>
+                </div>
+            </div>
+        </form>
+    </div>
 
 </section>
 
