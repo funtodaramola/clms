4543a951092af3d9362a6dfd2a7cfe594866e9e8
Funto Daramola <fundaramola2013@gmail.com>
2018-07-05 22:49:24 +0100

    slight changes to class and id

--- public/app/return.php
+++ public/app/return.php
@@ -1,6 +1,6 @@
 <?php include ('../../includes/layouts/header.php')?>
-        <section class="Issue-section">
-            <form action="Return.php" method="post">
+        <section class="return-section">
+            <form action="return.php" method="post">
                 <div class="form-issue">
                     <h1>Return Book</h1>
                     <hr>
@@ -10,12 +10,12 @@
                     <label for="libraryNo"><b>Library Number</b></label>
                     <input type="text" placeholder="Library Number..." name="libraryNo" class="input-field" required>
 
-                    <label for="datedue"><b>Date Returned</b></label>
-                    <input type="date" name="datedue" min="2018-01-01">
+<!--                     <label for="datedue"><b>Date Issued</b></label>
+                    <input type="date" name="datedue" min="2018-01-01"> -->
 
-                    <button type="submit" class="submit" name= "Return" value="Return"><span id="ReviewBtn">Return</span></button>
+                    <button type="submit" class="submit" name= "Return" value="Return"><span id="returnBtn">Return</span></button>
                 </div>
             </form>
             
-            </section>
+        </section>
 <?php include ('../../includes/layouts/footer.php'); ?>
