ccb463ae7868999c95a7b8fe4a93263275b8ace7
Funto Daramola <fundaramola2013@gmail.com>
2018-07-06 00:05:16 +0100

    finial style for add-admin form

--- public/app/manage.php
+++ public/app/manage.php
@@ -3,37 +3,32 @@
 <section class="manage-section">
     <div class="top"><h1>MANAGE ADMINS</h1></div>
     <div class="manage-action">
-        <button onclick="document.getElementById('add-admin').style.display='block'" class="manageBtn"><li>ADD ADMIN</li></button>
-        <button onclick="document.getElementById('add-admin').style.display='block'" class="manageBtn"><li>REMOVE ADMIN</li></button>
+        <button onclick="document.getElementById('add-admin').style.display='block'" onclick=repadding() class="manageBtn">
+            <li>ADD ADMIN</li>
+        </button>
+            <!-- code for adding admin -->
+            <div class="form-content" id="add-admin">
+                <form action="#" method="post" >
+                    <hr>			
+                    <label for="fullname"><b>Full name</b></label>
+                    <input type="text" placeholder="Enter Full name..." name="fullname" class="input-field" required>
+
+                    <label for="username"><b>Username</b></label>
+                    <input type="text" placeholder="Username will be generated..." name="username" class="input-field" required>
+
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="Password will be generated..." name="password" class="input-field" required>
+
+                    <div class="btns">
+                        <button type="button" onclick="document.getElementById('add-admin').style.display='none'" class="cancelbtn">Cancel</button>
+                        <button type="submit" class="submit">Add</button>
+                    </div>
+                </form>
+            </div>
+        <button class="manageBtn"><li>REMOVE ADMIN</li></button>
     </div>
     <div class="clear-fix"></div>
-
-
-    <!-- The Modal (contains the Add admin form) -->
-    <div id="add-admin" class="modal-form">
-        <!-- span to close the modal -->
-        <span onclick="document.getElementById('add-admin').style.display='none'" class="close-modal" title="Close Modal">&times;</span>
-        <form class="modal-content" action="#" method="post">
-            <div class="form-content">
-                <h1>New Admin</h1>
-                <hr>
-                <label for="fullname"><b>Full name</b></label>
-                <input type="text" placeholder="Enter Full name..." name="fullname" class="input-field" required>
-
-                <label for="username"><b>Username</b></label>
-                <input type="text" placeholder="Username will be generated..." name="username" class="input-field" required>
-
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Password will be generated..." name="password" class="input-field" required>
-
-                <div class="btns">
-                    <button type="button" onclick="document.getElementById('add-admin').style.display='none'" class="cancelbtn">Cancel</button>
-                    <button type="submit" class="submit">Add</button>
-                </div>
-            </div>
-        </form>
-    </div>
-
+                
 </section>
 
 
