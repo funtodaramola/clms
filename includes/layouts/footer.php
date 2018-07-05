7251d3748826d5325519b63178094f2ffda5504e
Funto Daramola <fundaramola2013@gmail.com>
2018-07-05 09:12:39 +0100

    restructured footer.php code

--- includes/layouts/footer.php
+++ includes/layouts/footer.php
@@ -2,50 +2,17 @@
     <span>&copy; Caleb University || Library</span>
     <div class="logout">
         <span>
-            <button id="myBtn" onclick="document.getElementById('account').style.display='block'" class="account">Account Settings</button>
-			<div id="account" class="modal-form">
-			
-    
-	<span onclick="document.getElementById('account').style.display='none'" class="close-modal" title="Close Modal">&times;</span>
-	<form class="modal-content" action="#" method="post">
-            <div class="form-content">
-                <h1>Update User Info</h1> 
-                <hr>				
-                <label for="username"><b>Username</b></label>
-                <input type="text" placeholder="New Username..." name="username" class="input-field" required>
-
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="New Password..." name="password" class="input-field" required>
-
-                <div class="btns">
-                    <button type="button" onclick="document.getElementById('account').style.display='none'" class="cancelbtn">Cancel</button>
-                    <button type="submit" class="submit">Update</button>
-                </div>
-            </div>
-        </form>
- 
-
-</div>
-
-
-
-            <!-- Button to open the modal -->
-            <button onclick="document.getElementById('add-admin').style.display='block'" class="add-admin">Add Admin</button>
-            <!-- TODO add manage admins page-->
-
-            <!-- <?php 
-            $username = $_POST["username"];
-            echo "Logged in as {$username}";
-            ?> -->
-            <!-- Need to fix this after connecting to database of registered users -->
+            <!-- Buttons to open the modal -->
+            <button id="myBtn" onclick="document.getElementById('account').style.display='block'" class="foot_btn">Account Settings</button>
+            <button onclick="document.getElementById('add-admin').style.display='block'" class="foot_btn">Add Admin</button>
+            <button class="foot_btn">Logout</button>
         </span>
-	
     </div>
+
     <!-- The Modal (contains the Add admin form) -->
     <div id="add-admin" class="modal-form">
-	
         <!-- span to close the modal -->
-            <span onclick="document.getElementById('add-admin').style.display='none'" class="close-modal" title="Close Modal">&times;</span>
+        <span onclick="document.getElementById('add-admin').style.display='none'" class="close-modal" title="Close Modal">&times;</span>
         <form class="modal-content" action="#" method="post">
             <div class="form-content">
                 <h1>New Admin</h1>
@@ -65,6 +32,33 @@
                 </div>
             </div>
         </form>
+    </div>
+
+<!-- The Modal (contains the Account settings form) -->
+    <div id="account" class="modal-form">
+        <!-- span to close the modal -->
+        <span onclick="document.getElementById('account').style.display='none'" class="close-modal" title="Close Modal">&times;</span>
+        <div class="modal-content">
+            <div class="account-head"><h1>Account Settings</h1></div>
+            
+                <div class="form-content">
+                    <form action="#" method="post">
+                    <h2>Change Username and Password</h2>
+                    <hr>			
+                    <label for="username"><b>Username</b></label>
+                    <input type="text" placeholder="New Username..." name="username" class="input-field" required>
+
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="New Password..." name="password" class="input-field" required>
+
+                    <div class="btns">
+                        <button type="button" onclick="document.getElementById('account').style.display='none'" class="cancelbtn">Cancel</button>
+                        <button type="submit" class="submit">Update</button>
+                    </div>
+                    </form>
+                </div>
+            
+        </div>
     </div>
 
 </footer>
