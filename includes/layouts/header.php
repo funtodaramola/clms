d504f0e7ff03240c75da2fc27e32230d74257933
Funto Daramola <fundaramola2013@gmail.com>
2018-07-06 10:57:29 +0100

    changed font-family Alegreya

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/app.css">
+    <link href='https://fonts.googleapis.com/css?family=Alegreya' rel='stylesheet'>
     <script src="../../public/js/jquery-3.3.1.min.js"></script>
     <script src="../../public/js/script.js"></script>
 	<script src="../../public/js/scripts.js"></script>
